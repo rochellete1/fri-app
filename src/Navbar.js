65f7d16e60b44c5d37105b9ce5905bbd90d49202
rockzy <rochelle_te@yahoo.com>
2022-12-16 22:44:47 +0800

    4th commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ function Navbar(){
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <Link className="nav-link active" aria-current="page" to="/home">Home</Link>
                         </li>
                         <li className="nav-item">
                         <Link className="nav-link" to="/features">Features</Link>
