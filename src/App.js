2f3056729ce8b3c141b1cb0ab298fbcfb955e958
rockzy <rochelle_te@yahoo.com>
2022-12-16 22:18:44 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Navbar/>
       <div>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/fri-app" element={<Home/>}/>
           <Route path="/home" element={<Home/>}/>
           <Route path="/features" element={<Features/>}/>
           <Route path="/pricing" element={<Pricing/>}/>
