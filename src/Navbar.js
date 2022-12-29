206f5cc52a70ff8c09bc8b2639d02ee03fcb2cfa
rockzy <rochelle_te@yahoo.com>
2022-12-29 17:24:38 +0800

    first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ function Navbar(){
         <div>
             <nav className="navbar navbar-expand-lg bg-primary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Website</Link>
+                    <Link className="navbar-brand" to="/home">Website</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
